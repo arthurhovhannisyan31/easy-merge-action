1c5db188eb30859b9ae5331c62caa8e70647c7c1
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2025-02-15 15:15:32 +0400

    update script

--- src/index.ts
+++ src/index.ts
@@ -22,49 +22,49 @@ try {
 
   const octokit = github.getOctokit(PAT);
 
-  const {
-    data: tagsList
-  } = await octokit.rest.repos.listTags({
-    owner: OWNER,
-    repo: REPO
-  });
-  const latestTag = tagsList[0];
-  console.log({
-    latestTag
-  });
-  console.log("compare", latestTag.name, version);
-  // parse tags
-  // validate provided version
-  // check if provided version is not at latest tag
-
-  const latestRelease = octokit.rest.repos.getLatestRelease({
-    owner: OWNER,
-    repo: REPO
-  });
-
-  console.log({
-    latestRelease
-  });
-
-  const {
-    data: mainBranch
-  } = await octokit.rest.repos.getBranch({
-    owner: OWNER,
-    repo: REPO,
-    branch: "main"
-  });
-  const {
-    data: devBranch
-  } = await octokit.rest.repos.getBranch({
-    owner: OWNER,
-    repo: REPO,
-    branch: "develop"
-  });
-
-  console.log({
-    mainBranch,
-    devBranch
-  });
+  // const {
+  //   data: tagsList
+  // } = await octokit.rest.repos.listTags({
+  //   owner: OWNER,
+  //   repo: REPO
+  // });
+  // const latestTag = tagsList[0];
+  // console.log({
+  //   latestTag
+  // });
+  // console.log("compare", latestTag.name, version);
+  // // parse tags
+  // // validate provided version
+  // // check if provided version is not at latest tag
+  //
+  // const latestRelease = octokit.rest.repos.getLatestRelease({
+  //   owner: OWNER,
+  //   repo: REPO
+  // });
+  //
+  // console.log({
+  //   latestRelease
+  // });
+  //
+  // const {
+  //   data: mainBranch
+  // } = await octokit.rest.repos.getBranch({
+  //   owner: OWNER,
+  //   repo: REPO,
+  //   branch: "main"
+  // });
+  // const {
+  //   data: devBranch
+  // } = await octokit.rest.repos.getBranch({
+  //   owner: OWNER,
+  //   repo: REPO,
+  //   branch: "develop"
+  // });
+  //
+  // console.log({
+  //   mainBranch,
+  //   devBranch
+  // });
   // get tag for a commit sha
 } catch (error: unknown) {
   core.setFailed((error as Error).message);
