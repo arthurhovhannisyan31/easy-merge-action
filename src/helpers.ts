60fc487f0ae603a08f2f9ac254a9e725892dad1f
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2025-02-27 01:19:11 +0400

    update script

--- src/helpers.ts
+++ src/helpers.ts
@@ -7,6 +7,7 @@ import type { components } from "@octokit/openapi-types";
 
 type GitTag = components["schemas"]["git-tag"];
 type Tag = components["schemas"]["tag"];
+type Release = components["schemas"]["release"];
 
 export const validateBranchesMerge = async (
   octokit: InstanceType<typeof GitHub>,
@@ -106,4 +107,57 @@ export const syncBranches = async (
   await exec.exec("git", ["checkout", head, "-q"]);
   await exec.exec("git", ["merge", base, "--ff", "-q"]);
   await exec.exec("git", ["push", "origin", "-f", "-q"]);
+};
+
+export const createRelease = async (
+  octokit: InstanceType<typeof GitHub>,
+  sourceBranchName: string,
+  previousTag: Tag,
+  tagName: string
+): Promise<Release> => {
+  const {
+    owner,
+    repo
+  } = github.context.repo;
+  const {
+    data: commits
+  } = await octokit.rest.repos.listCommits({
+    owner,
+    repo,
+    sha: sourceBranchName
+  });
+
+  const previousTagIndex = commits.findIndex(({
+    sha
+  }) => sha === previousTag.commit.sha);
+  if (!Number.isFinite(previousTagIndex)) {
+    throw new Error("Failed creating release. add more details");
+  }
+  /* Filter out merge commits from edges */
+  const filteredCommits = commits.slice(1, previousTagIndex);
+
+  const commitsNotes = filteredCommits.map(({
+    commit,
+    author
+  }) => `${author?.login} ${commit.message}`).join("\n");
+
+  const {
+    data: releaseNotes
+  } = await octokit.rest.repos.generateReleaseNotes({
+    owner,
+    repo,
+    tag_name: tagName,
+  });
+
+  const {
+    data: release
+  } = await octokit.rest.repos.createRelease({
+    owner,
+    repo,
+    tag_name: tagName,
+    name: `Release ${releaseNotes.name}`,
+    body: `${commitsNotes}\n\n${releaseNotes.body}`
+  });
+
+  return release;
 };
