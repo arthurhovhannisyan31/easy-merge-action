da8c381cc48e7e6aeed5e2f83cd0ee67bf3e3783
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2025-05-01 18:50:41 +0400

    update scripts and build files

---
+++ src/helpers.ts
@@ -0,0 +1,151 @@
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as github from "@actions/github";
+import { coerce, inc, type ReleaseType, type SemVer, valid } from "semver";
+
+import { DEFAULT_VERSION } from "./constants";
+
+import type {
+  GitTag,
+} from "./types";
+import type { GitHub } from "@actions/github/lib/utils";
+
+export const validateBranchesMerge = async (
+  octokit: InstanceType<typeof GitHub>,
+  base: string,
+  head: string
+): Promise<void> => {
+  const {
+    owner,
+    repo
+  } = github.context.repo;
+  const {
+    data: compareCommits
+  } = await octokit.rest.repos.compareCommits({
+    owner,
+    repo,
+    base,
+    head,
+  });
+
+  if (compareCommits.status !== "ahead") {
+    throw new Error(
+      `The '${head}' branch is not ahead of '${base}' branch. Rebase the '${head}' branch first.`
+    );
+  }
+
+  core.info("✔ Branches are valid for merge");
+};
+
+export const getNextTagName = async (
+  octokit: InstanceType<typeof GitHub>,
+  releaseType: ReleaseType
+): Promise<string> => {
+  const {
+    owner,
+    repo
+  } = github.context.repo;
+  const {
+    data: tagsList
+  } = await octokit.rest.repos.listTags({
+    owner,
+    repo,
+  });
+  const latestTag = tagsList?.[0];
+
+  const latestTagName = latestTag?.name ?? DEFAULT_VERSION;
+
+  if (!valid(latestTagName)) {
+    throw new Error("Latest tag version is not valid, check git tags");
+  }
+
+  const nextTagName = inc(coerce(latestTagName) as SemVer, releaseType) as string;
+
+  if (!nextTagName) {
+    throw new Error("Failed creating new tag");
+  }
+
+  core.info(`✔ Next tag name created: ${nextTagName}`);
+
+  return `v${nextTagName}`;
+};
+
+export const createTag = async (
+  octokit: InstanceType<typeof GitHub>,
+  tag: string,
+  commitSha: string
+): Promise<GitTag> => {
+  const {
+    owner,
+    repo
+  } = github.context.repo;
+
+  const {
+    data: newTag,
+  } = await octokit.rest.git.createTag({
+    owner,
+    repo,
+    tag,
+    message: "",
+    object: commitSha,
+    type: "commit"
+  });
+
+  await octokit.rest.git.createRef({
+    owner,
+    repo,
+    ref: `refs/tags/${tag}`,
+    sha: newTag.sha
+  });
+
+  return newTag;
+};
+
+export const syncBranches = async (
+  base: string,
+  head: string
+): Promise<void> => {
+  // no existing api for --no-ff merge
+  await exec.exec("git", ["fetch", "-q"]);
+  // need to populate targetBranch in local context
+  await exec.exec("git", ["checkout", base, "-q"]);
+  await exec.exec("git", ["checkout", head, "-q"]);
+  await exec.exec("git", ["merge", base, "--ff", "-q"]);
+  await exec.exec("git", ["push", "origin", "-f", "-q"]);
+};
+
+export const processMerge = async (
+  octokit: InstanceType<typeof GitHub>,
+  base: string,
+  head: string,
+  tagName: string
+): Promise<void> => {
+  const {
+    owner,
+    repo
+  } = github.context.repo;
+
+  const {
+    data: mergeCommit
+  } = await octokit.rest.repos.merge({
+    owner,
+    repo,
+    base,
+    head,
+    commit_message: `Release ${tagName}`
+  });
+  core.info(`✔ Branches '${base}' and '${head}' are merged`);
+
+  await createTag(
+    octokit,
+    tagName,
+    mergeCommit.sha
+  );
+  core.info(`✔ Tag ${tagName} assigned to merge commit`);
+
+  await syncBranches(
+    base,
+    head
+  );
+  core.info(`✔ Branch '${head}' is synced with '${base}'`);
+};
---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+import {
+  getNextTagName,
+  processMerge,
+  validateBranchesMerge
+} from "./helpers";
+
+import type {
+  ReleaseType,
+} from "semver";
+
+try {
+  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+
+  if (!GITHUB_TOKEN) {
+    throw new Error("Failed reading access token");
+  }
+
+  const sourceBranchName = core.getInput("source_branch", { required: true });
+  const targetBranchName = core.getInput("target_branch", { required: true });
+  const releaseType = core.getInput("release_type", { required: true }) as ReleaseType;
+
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  await validateBranchesMerge(
+    octokit,
+    targetBranchName,
+    sourceBranchName,
+  );
+
+  const tagName = await getNextTagName(
+    octokit,
+    releaseType
+  );
+
+  await processMerge(
+    octokit,
+    targetBranchName,
+    sourceBranchName,
+    tagName
+  );
+
+  core.setOutput("release_tag", tagName);
+} catch (error: unknown) {
+  core.setFailed(`⚠️ ${(error as Error).message}`);
+}
+
+// 1
